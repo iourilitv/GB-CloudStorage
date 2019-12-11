6758d2680cae630495ed6c047f5f050a6e5778c1
iourilitv <iourilitv1965@gmail.com>
2019-12-11 08:31:17 +0300

    Moved all code which does not belong to the main method into the new TCPClient class. All works correctly. Solved the tasks: "The client sends to the server two messageObjects with a different AbstractMessage objects as a parameter(CommandMessage with data from a file and AuthMessage with login and password) and the server receives it all, deserializes and processes them" "The client sends to the server an messageObject with a AbstractMessage object as a parameter(CommandMessage with data from a file) and the server receives it all, deserializes and create a copy of the client's file"; "The client sends to the server an object with some data from a file and the server receives it and deserializes"; "on the client side, read a file any type by buffer input stream and send it to the server byte to byte"; "on the server side, the received from the client bytes are written into a file by buffer output stream"; "the server received a sequence of bytes sent by the client".

---
+++ client/src/main/java/TCPClient.java
@@ -0,0 +1,82 @@
+import messages.AuthMessage;
+import messages.FileMessage;
+import utils.CommandMessage;
+import utils.handlers.ObjectHandler;
+import utils.handlers.ServiceCommandHandler;
+import utils.handlers.UploadCommandHandler;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.PrintStream;
+
+public class TCPClient implements TCPConnectionListener {
+    //инициируем константу IP адреса сервера(здесь - адрес моего ноута в домашней локальной сети)
+    private static final String IP_ADDR = "192.168.1.103";//89.222.249.131(внешний белый адрес)
+    //инициируем константу порта соединения
+    private static final int PORT = 8189;
+    //инициируем переменную для печати сообщений в консоль
+    private final PrintStream log = System.out;
+    //объявляем переменную сетевого соединения
+    private TCPConnection connection;
+    //инициируем строку названия директории для хранения файлов клиента
+    private final String storageDir = "storage/client_storage";
+    //объявляем объект сообщения(команды)
+    private CommandMessage messageObject;
+    //объявляем объект обработчика сообщений(команд)
+    private ObjectHandler objectHandler;
+
+    public TCPClient() {
+        //инициируем объект обработчика сообщений(команд)
+        objectHandler = new ObjectHandler();
+        try {
+            //инициируем переменную сетевого соединения
+            //устанавливаем соединение при открытии окна
+            connection = new TCPConnection(this, IP_ADDR, PORT);
+        } catch (IOException e) {
+            printMsg("Connection exception: " + e);
+        }
+    }
+
+    public void send () throws IOException {
+        connection.sendMessageObject(new CommandMessage(CommandMessage.CMD_MSG_REQUEST_FILE_UPLOAD,
+                new UploadCommandHandler(new FileMessage(storageDir, "file1.txt"))));
+
+        connection.sendMessageObject(new CommandMessage(CommandMessage.CMD_MSG_REQUEST_AUTH,
+                new ServiceCommandHandler(new AuthMessage("login1", "pass1"))));
+    }
+
+    @Override
+    public void onConnectionReady(TCPConnection tcpConnection) {
+        printMsg("Connection ready...");
+    }
+
+    @Override
+    public void onDisconnect(TCPConnection tcpConnection) {
+        printMsg("Connection close");
+    }
+
+    @Override
+    public void onException(TCPConnection tcpConnection, Exception e) {
+        printMsg("Connection exception: " + e);
+    }
+
+    @Override
+    public void onReceiveObject(TCPConnection tcpConnection, ObjectInputStream ois) {
+        //десериализуем объект сообщения(команды)
+        try {
+            messageObject = (CommandMessage) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        //распознаем и обрабатываем полученный объект сообщения(команды)
+        objectHandler.recognizeAndArrangeMessageObject(messageObject, storageDir);
+    }
+
+    public String getStorageDir() {
+        return storageDir;
+    }
+
+    private synchronized void printMsg(String msg){
+        log.append(msg).append("\n");
+    }
+}
