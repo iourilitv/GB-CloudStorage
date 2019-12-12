a16ea36af12bfae66f6bce5f43b7d1f245224b4b
iourilitv <iourilitv1965@gmail.com>
2019-12-12 12:30:59 +0300

    Deleted old code in the command handlers classes of the server modules. All work correctly. The following scenarios work: *Scenario "upload a file":  - The client sends to the server a CommandMessage with a request "upload the file from the client directory to the storage directory" with the file data inside;  - The server reads the file data, saves it into a new file and sends back a CommandMessage about a result; *Scenario "download a file":  - The client sends to the server a CommandMessage with a request "download the file from the storage directory to the client directory";  - The server reads the file and sends back a CommandMessage with the file data;  - The client saves the file.

--- server/src/main/java/utils/handlers/FileCommandHandler.java
+++ server/src/main/java/utils/handlers/FileCommandHandler.java
@@ -1,10 +1,8 @@
 package utils.handlers;
 
-import messages.Commands;
 import messages.FileFragmentMessage;
 import messages.FileMessage;
 import tcp.TCPServer;
-import utils.CommandMessage;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -37,25 +35,14 @@ public class FileCommandHandler extends CommandHandler{
         return fileFragmentMessage;
     }
 
-//    /**
-//     * Метод сохраняет полученный от клиента целый файл
-//     * в заданную директорию сетевого хранилища(сервера)
-//     * @param fromDir - заданная директория(папка) сервера
-//     * @param toDir - заданная директория(папка) клиента
-//     * @param fileMessage - объект файла, полученного от сервера
-//     * @throws IOException - исключение ввода-вывода
-//     */
-//    public boolean saveUploadedFile(String fromDir, String toDir, FileMessage fileMessage) throws IOException {
-//        System.out.println("(Server)FileCommandHandler.saveDownloadedFile - fileMessage.getFilename(): " +
-//                fileMessage.getFilename() +
-//                ". Arrays.toString(fileMessage.getData()): " +
-//                Arrays.toString(fileMessage.getData()));
-//
-//        Files.write(Paths.get(toDir, fileMessage.getFilename()),
-//                fileMessage.getData(), StandardOpenOption.CREATE);
-//
-//        return true;//FIXME
-//    }
+    /**
+     * Метод сохраняет полученный от клиента целый файл
+     * в заданную директорию сетевого хранилища(сервера)
+     * @param server - объект сервера
+     * @param toDir - заданная директория(папка) клиента в сетевом хранилище
+     * @param fileMessage - объект файлового сообщения с данными файла
+     * @return true, если файл сохранен без ошибок
+     */
     public boolean saveUploadedFile(TCPServer server, String toDir, FileMessage fileMessage) {
         System.out.println("(Server)FileCommandHandler.saveDownloadedFile - fileMessage.getFilename(): " +
                 fileMessage.getFilename() +
@@ -73,29 +60,13 @@ public class FileCommandHandler extends CommandHandler{
         return true;
     }
 
-//    /**
-//     * Метод читает данные из целого файла в заданной директорию сетевого хранилища и
-//     * отпраляет клиенту объект сообщения с данными файла.
-//     * @param server - объект сервера
-//     * @param fromDir - заданная директория(папка) сервера
-//     * @param toDir - заданная директория(папка) клиента
-//     * @param filename - имя файла
-//     * @throws IOException - исключение ввода-вывода
-//     */
-//    public void downloadAndSendFile(TCPServer server, String fromDir, String toDir, String filename) throws IOException {
-//        //FIXME добавить проверку на наличие файла в директории?
-//
-//        System.out.println("(Server)FileCommandHandler.downloadFile - fileMessage.getFilename(): " +
-//                fileMessage.getFilename() +
-//                ". Arrays.toString(fileMessage.getData()): " +
-//                Arrays.toString(fileMessage.getData()));
-//
-//        FileMessage fileMessage = new FileMessage(fromDir, toDir, filename);
-//        fileMessage.readFileData();//TODO
-//
-//        server.sendToClient("login1", new CommandMessage(Commands.SERVER_RESPONSE_FILE_DOWNLOAD_OK,
-//                fileMessage));
-//    }
+    /**
+     * Метод читает данные из целого файла в заданной директорию сетевого хранилища и
+     * отправляет клиенту объект сообщения с данными файла.
+     * @param server - объект сервера
+     * @param fileMessage - объект файлового сообщения с данными файла
+     * @return true, если файл скачан без ошибок
+     */
     public boolean downloadFile(TCPServer server, FileMessage fileMessage) {
         //FIXME добавить проверку на наличие файла в директории?
 
--- server/src/main/java/utils/handlers/ObjectHandler.java
+++ server/src/main/java/utils/handlers/ObjectHandler.java
@@ -27,55 +27,10 @@ public class ObjectHandler {
         this.server = server;
     }
 
-//    public void recognizeAndArrangeMessageObject(CommandMessage messageObject) {
-//        try {
-//            FileMessage fileMessage;
-//            FileCommandHandler fileCommandHandler;
-//            String clientDir;
-//            String storageDir;
-//
-//            //выполняем операции в зависимости от типа полученного сообщения(команды)
-//            switch (messageObject.getCommand()){
-//                //обрабатываем полученный от клиента запрос на загрузку(сохранение) файла в облачное хранилище
-//                case Commands.REQUEST_SERVER_FILE_UPLOAD:
-//                    fileMessage = (FileMessage) messageObject.getMessageObject();
-//                    fileCommandHandler = new FileCommandHandler(fileMessage);
-//                    clientDir = fileMessage.getFromDir();
-//                    storageDir = fileMessage.getToDir();
-//                    int command = Commands.SERVER_RESPONSE_FILE_UPLOAD_ERROR;
-//                    //если сохранение прошло удачно
-//                    if(fileCommandHandler.saveUploadedFile(clientDir, storageDir, fileMessage)){
-//                        //проверяем сохраненный файл по контрольной сумме//FIXME
-//                        if(true){
-//                            //отправляем сообщение на сервер: подтверждение, что все прошло успешно
-//                            command = Commands.SERVER_RESPONSE_FILE_UPLOAD_OK;
-//                        }
-//                    }
-//                    //создаем объект файлового сообщения
-//                    fileMessage = new FileMessage(storageDir, clientDir, fileMessage.getFilename());
-//                    //отправляем объект сообщения(команды) клиенту
-//                    server.sendToClient("login1", new CommandMessage(command, fileMessage));
-//                    break;
-//                //обрабатываем полученный от клиента запрос на скачивание файла из облачного хранилища
-//                case Commands.REQUEST_SERVER_FILE_DOWNLOAD:
-//                    fileMessage = (FileMessage) messageObject.getMessageObject();
-//                    fileCommandHandler = new FileCommandHandler(fileMessage);
-//                    storageDir = fileMessage.getFromDir();
-//                    clientDir = fileMessage.getToDir();
-//                    fileCommandHandler.downloadAndSendFile(server, storageDir, clientDir, fileMessage.getFilename());
-//                    break;
-//                //обрабатываем полученный от клиента запрос на авторизацию в облачное хранилище
-//                case Commands.REQUEST_SERVER_AUTH:
-////                    ServiceCommandHandler serviceCommandHandler = (ServiceCommandHandler) messageObject.getCommandHandler();
-//                    AuthMessage authMessage = (AuthMessage) messageObject.getMessageObject();
-//                    ServiceCommandHandler serviceCommandHandler = new ServiceCommandHandler(authMessage);
-//                    serviceCommandHandler.authorizeUser();
-//                    break;
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
+    /**
+     * Метот распознает тип команды и обрабатывает ее.
+     * @param messageObject - объект сообщения(команды)
+     */
     public void recognizeAndArrangeMessageObject(CommandMessage messageObject) {
         //выполняем операции в зависимости от типа полученного сообщения(команды)
         switch (messageObject.getCommand()) {
@@ -107,35 +62,6 @@ public class ObjectHandler {
      * сетевом хранилище.
      * @param messageObject - объект сообщения(команды)
      */
-//    private void uploadFile(CommandMessage messageObject) {
-//        //вынимаем объект файлового сообщения из объекта сообщения(команды)
-//        fileMessage = (FileMessage) messageObject.getMessageObject();
-//        //инициируем объект файлового хендлера
-//        fileCommandHandler = new FileCommandHandler(fileMessage);
-//        //вынимаем заданную клиентскую директорию из объекта сообщения(команды)
-//        clientDir = fileMessage.getFromDir();
-//        //вынимаем заданную директорию сетевого хранилища из объекта сообщения(команды)
-//        storageDir = fileMessage.getToDir();
-//        //инициируем переменную типа команды(по умолчанию - ответ об ошибке)
-//        int command = Commands.SERVER_RESPONSE_FILE_UPLOAD_ERROR;
-//        try {
-//            //если сохранение прошло удачно
-//            if(fileCommandHandler.saveUploadedFile(clientDir, storageDir, fileMessage)){
-//                //проверяем сохраненный файл по контрольной сумме//FIXME
-//                if(true){
-//                    //отправляем сообщение на сервер: подтверждение, что все прошло успешно
-//                    command = Commands.SERVER_RESPONSE_FILE_UPLOAD_OK;
-//                }
-//            }
-//        } catch (IOException e) {
-//            server.printMsg("Something went wrong during the upload process!");
-//            e.printStackTrace();
-//        }
-//        //создаем объект файлового сообщения
-//        fileMessage = new FileMessage(storageDir, clientDir, fileMessage.getFilename());
-//        //отправляем объект сообщения(команды) клиенту
-//        server.sendToClient("login1", new CommandMessage(command, fileMessage));
-//    }
     private void uploadFile(CommandMessage messageObject) {
         //вынимаем объект файлового сообщения из объекта сообщения(команды)
         fileMessage = (FileMessage) messageObject.getMessageObject();
@@ -155,12 +81,6 @@ public class ObjectHandler {
                 command = Commands.SERVER_RESPONSE_FILE_UPLOAD_OK;
             }
         }
-//        try {
-//
-//        } catch (IOException e) {
-//            server.printMsg("Something went wrong during the upload process!");
-//            e.printStackTrace();
-//        }
         //создаем объект файлового сообщения
         fileMessage = new FileMessage(storageDir, clientDir, fileMessage.getFilename());
         //отправляем объект сообщения(команды) клиенту
@@ -195,74 +115,5 @@ public class ObjectHandler {
         }
         //отправляем объект сообщения(команды) клиенту
         server.sendToClient("login1", new CommandMessage(command, fileMessage));
-
-//        try {
-//        } catch (IOException e) {
-//            server.printMsg("Something went wrong during the download process!");
-//            e.printStackTrace();
-//        }
     }
 }
-
-//TODO Delete at the finish.
-
-//    public void recognizeAndArrangeMessageObject(AbstractMessage messageObject, String storageDir) {
-//        try {
-//            //выполняем операции в зависимости от типа полученного сообщения(команды)
-//            switch (messageObject.getClass().getSimpleName()){
-//                case "utils.CommandMessage":
-//                    utils.CommandMessage commandMessage = (utils.CommandMessage) messageObject;
-//                    System.out.println("ByteServer.onReceiveObject - commandMessage.getFilename(): " +
-//                            commandMessage.getFilename() +
-//                            ". Arrays.toString(commandMessage.getData()): " +
-//                            Arrays.toString(commandMessage.getData()));
-//
-//                    Files.write(Paths.get(storageDir, commandMessage.getFilename()),
-//                            commandMessage.getData(), StandardOpenOption.CREATE);
-//                    break;
-//                case "AuthMessage":
-//                    AuthMessage authMessage = (AuthMessage) messageObject;
-//                    System.out.println("ByteServer.onReceiveObject - commandMessage.getLogin(): " +
-//                            authMessage.getLogin() +
-//                            ". commandMessage.getPassword(): " + authMessage.getPassword());
-//                    break;
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//    public void recognizeAndArrangeMessageObject(AbstractMessage messageObject, String storageDir) {
-//        try {
-//            //выполняем операции в зависимости от типа полученного сообщения(команды)
-//            switch (messageObject.getClass().getSimpleName()){
-////                case "FileFragmentMessage":
-////                    FileFragmentMessage fileFragmentMessage = (FileFragmentMessage) messageObject;
-////                    System.out.println("Server.onReceiveObject - fileFragmentMessage.getFilename(): " +
-////                            fileFragmentMessage.getFilename() +
-////                            ". Arrays.toString(fileFragmentMessage.getData()): " +
-////                            Arrays.toString(fileFragmentMessage.getData()));
-////
-////                    Files.write(Paths.get(storageDir, fileFragmentMessage.getFilename()),
-////                            fileFragmentMessage.getData(), StandardOpenOption.CREATE);
-////                    break;
-//                case "FileMessage":
-//                    FileMessage fileMessage = (FileMessage) messageObject;
-//                    System.out.println("Server.onReceiveObject - fileMessage.getFilename(): " +
-//                            fileMessage.getFilename() +
-//                            ". Arrays.toString(fileMessage.getData()): " +
-//                            Arrays.toString(fileMessage.getData()));
-//
-//                    Files.write(Paths.get(storageDir, fileMessage.getFilename()),
-//                            fileMessage.getData(), StandardOpenOption.CREATE);
-//                    break;
-//                case "AuthMessage":
-//                    AuthMessage authMessage = (AuthMessage) messageObject;
-//                    System.out.println("Server.onReceiveObject - authMessage.getLogin(): " +
-//                            authMessage.getLogin() +
-//                            ". authMessage.getPassword(): " + authMessage.getPassword());
-//                    break;
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
