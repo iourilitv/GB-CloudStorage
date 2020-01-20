4a9f86be34c25f28d1567ba7bfa90ed6e53d340c
iourilitv <iourilitv1965@gmail.com>
2020-01-20 07:38:21 +0300

    Updating after lesson 8 Code Review. GUI p.13. Fixed the db connection trouble temporarily by replacing db with HashMap in the class [server]UsersDB and in the couple methods in the class UsersAuthController.

--- server/src/main/java/jdbc/UsersDB.java
+++ server/src/main/java/jdbc/UsersDB.java
@@ -1,14 +1,52 @@
 package jdbc;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * The class is a temporarily storage for users data. //FIXME move it all to DB
  */
 public class UsersDB {
+    private static final UsersDB ownInstance = new UsersDB();
 
-    public static final String[][] users = {
-                                {"login1", "pass1"},
-                                {"login2", "pass2"},
-                                {"login3", "pass3"}
-                            };
+    public static UsersDB getOwnInstance() {
+        registeredUsers.put("login1", "pass1");
+        registeredUsers.put("login2", "pass2");
+        return ownInstance;
+    }
 
+    //объявляем множество зарегистрированных клиентов <логин, пароль>
+    private static final Map<String, String> registeredUsers = new HashMap<>();
+
+    /**
+     * Метод добавляет нового пользователя в множество зарегистрированных в облачном хранилище.
+     * @param login - логин нового пользователя
+     * @param password - пароль нового пользователя
+     * @return - результат добавления и проверки нового пользователя в множество
+     */
+    public boolean addUserIntoMap(String login, String password){
+        //добавляем пользователя в множество
+        registeredUsers.put(login, password);
+
+//        System.out.println("UsersDB.addUserIntoMap() - " +
+////                "registeredUsers.put(login, password): " + registeredUsers.put(login, password) +
+//                ", registeredUsers.get(login): " + registeredUsers.get(login) +
+////                ", registeredUsers.get(login).equals(password): " + registeredUsers.get(login).equals(password) +
+//                ", registeredUsers.toString(): " + registeredUsers.toString());
+
+        //возвращаем результат проверки действительно ли пользователь правильно добавлен в множество
+        return registeredUsers.get(login) != null && registeredUsers.get(login).equals(password);
+    }
+
+    /**
+     * Метод проверяет не зарегистрирован ли уже кто-то с таким логином.
+     * @param login - проверяемый логин пользователя
+     * @return - результат проверки
+     */
+    public boolean isUserExistInMap(String login) {
+        System.out.println("UsersDB.isUserExistInMap() - " +
+                "registeredUsers.containsKey(login): " + registeredUsers.containsKey(login) +
+                ", registeredUsers.toString(): " + registeredUsers.toString());
+        return registeredUsers.containsKey(login);
+    }
 }
