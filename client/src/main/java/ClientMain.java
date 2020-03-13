30e585c16533010c51e49dc777f040d9fbbccd50
iourilitv <iourilitv1965@gmail.com>
2020-03-13 11:15:42 +0300

    Updating after the project acceptance. Deleted old code.

--- client/src/main/java/ClientMain.java
+++ client/src/main/java/ClientMain.java
@@ -38,7 +38,6 @@ public class ClientMain extends Application {
         primaryStage.setTitle("The Cloud Storage by LYS");
         Scene scene = new Scene(root, 1024, 500);
         primaryStage.setScene(scene);
-//        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
